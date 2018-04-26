c478ddb46caa578564933f9348dda73858741698
Tuev Max <tuev@surfstudio.ru>
2018-04-26 18:16:58 +0300

    test

--- src/ru/surfstudio/ci/Test.groovy
+++ src/ru/surfstudio/ci/Test.groovy
@@ -1,7 +1,13 @@
 package ru.surfstudio.ci
 
-class Test {
-    static void testPipline(script) {
+class Test implements Serializable {
+    private script;
+
+    Test(script) {
+        this.script = script
+    }
+
+    void testPipline() {
         script.node("android.node"){
             script.stage("test stage") {
                 script.echo "Home ${script.env.HOME}"
