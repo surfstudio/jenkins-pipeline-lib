4ac9ef726e5e82cc81d84bc7de54a05e4a005c6d
Tuev Max <tuev@surfstudio.ru>
2018-05-04 18:33:10 +0300

    fix publish ui tests

--- src/ru/surfstudio/ci/CommonUtil.groovy
+++ src/ru/surfstudio/ci/CommonUtil.groovy
@@ -59,7 +59,7 @@ class CommonUtil {
         try {
             body()
         } catch (e){
-            script.sh "Ignored exception: ${e.message}"
+            script.echo "Ignored exception: ${e.message}"
         }
     }
 
