6f76debde124e26345d4060059ae37af8edc0cf1
Gregory Berngardt <gregoryvit@gmail.com>
2018-05-21 20:27:08 +0300

    Added keychain credentials support

--- src/ru/surfstudio/ci/stage/body/CommoniOSStages.groovy
+++ src/ru/surfstudio/ci/stage/body/CommoniOSStages.groovy
@@ -5,8 +5,17 @@ import ru.surfstudio.ci.CommonUtil
 class CommoniOSStages {
 
     def static buildStageBodyiOS(Object script) {
-        script.sh "make init"
-        script.sh "make build"
+        withCredentials([
+            string(credentialsId: 'Jenkins keychain pass', variable: 'KEYCHAINPASS'),
+            file(credentialsId: 'iOSDevIvanSmetaninVM.p12', variable: 'DEVELOPER_P12_KEY')
+        ]) {
+
+            script.sh "security -v unlock-keychain -p $KEYCHAINPASS"
+            script.sh "security import \"$DEVELOPER_P12_KEY\" -P \"\""
+            
+            script.sh "make init"
+            script.sh "make build"
+        }
     }
 
     def static unitTestStageBodyiOS(Object script) {
