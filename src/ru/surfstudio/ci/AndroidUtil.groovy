16f2c564c4037bbc767efd688ca17230d012b141
sloskutov-surf <loskutov@surfstudio.ru>
2018-05-30 20:01:26 +0300

    Undo add "-wipe-data" option to emulator launch command for calabash

--- src/ru/surfstudio/ci/AndroidUtil.groovy
+++ src/ru/surfstudio/ci/AndroidUtil.groovy
@@ -9,7 +9,7 @@ class AndroidUtil {
             script.sh "$ADB devices"
             script.sh "$EMULATOR -list-avds"
             script.lock(avdName) { //блокируем эмулятор
-                script.sh "$EMULATOR -avd avd-main -wipe-data -no-window -skin 1440x2560 &"
+                script.sh "$EMULATOR -avd avd-main -no-window -skin 1440x2560 &"
                 script.timeout(time: 120, unit: 'SECONDS') { //2мин ждем запуск девайса
                     script.sh "$ADB wait-for-device"
                 }
