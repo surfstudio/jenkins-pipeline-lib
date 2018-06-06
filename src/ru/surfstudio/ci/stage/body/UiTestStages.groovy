71bb9ed898f852ced33d4ad83c2e62ea4cfd87a9
Игорь Толубаев <tolubaev@surfstudio.co>
2018-06-06 16:32:33 +0000

    UiTestStages.groovy edited online with Bitbucket

--- src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
+++ src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
@@ -97,10 +97,9 @@ class UiTestStages {
                 script.sh 'security -v unlock-keychain -p $KEYCHAIN_PASS'
                 script.sh 'security import "$DEVELOPER_P12_KEY" -P ""'
                 
-                script.sh "make init"   
-                script.sh "ls" 
-                script.sh "cd .. &&  ls && bundle update "
-                script.sh "cd .. && yes | bundle exec calabash-ios setup"    
+                script.sh "make init"    
+                script.sh "cd .. && bundle install"
+                script.sh "cd .. && echo -ne 'yes \n' | bundle exec calabash-ios setup ${sourcesDir}"    
                 script.sh "xcodebuild -workspace MDK.xcworkspace -scheme MDK-cal -allowProvisioningUpdates -sdk iphonesimulator11.4 -derivedDataPath ${sourcesDir}"
                 script.sh "open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/"
                 script.sh "xcrun simctl install booted ${sourcesDir}/Build/Products/Debug-iphonesimulator/*-cal.app"
