6481bddadd5e085d7a9f6a71526c7ebc08fc46c3
Gregory Berngardt <gregoryvit@gmail.com>
2018-06-15 00:25:02 +0300

    Фикс синтаксиса

--- src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
+++ src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
@@ -147,9 +147,11 @@ class UiTestStages {
                              String outputHtmlFile,
                              String outputJsonFile) {
             
+            def simulatorIdentifierFile = "currentsim"
+
             script.echo "Setting up simulator ..."
-            script.sh "xcrun simctl create \"MyTestiPhone\" \"${device}\" \"${iosVersion}\" > currentsim"        
-            script.sh "xcrun simctl boot \$(cat currentsim)"
+            script.sh "xcrun simctl create \"MyTestiPhone\" \"${device}\" \"${iosVersion}\" > ${simulatorIdentifierFile}"        
+            script.sh "xcrun simctl boot \$(cat ${simulatorIdentifierFile})"
             script.sh "xcrun simctl install booted ${resultDerivedDataPath}/Build/Products/Debug-iphonesimulator/*.app"
             
             script.echo "Tests started"
@@ -159,10 +161,10 @@ class UiTestStages {
             }
             
             try {
-                script.sh "APP_BUNDLE_PATH=${derivedDataPath}/Build/Products/Debug-iphonesimulator/*.app DEVICE_TARGET=\$(cat ${simulatorIdentificationFile} bundle exec cucumber -p ios ${featuresDir}/${featureFile} -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile} -f pretty"
+                script.sh "APP_BUNDLE_PATH=${derivedDataPath}/Build/Products/Debug-iphonesimulator/*.app DEVICE_TARGET=\$(cat ${simulatorIdentifierFile}) bundle exec cucumber -p ios ${featuresDir}/${featureFile} -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile} -f pretty"
             } finally {
                 script.echo "Removing simulator ..."
-                script.sh "xcrun simctl delete \$(cat currentsim)"
+                script.sh "xcrun simctl delete \$(cat ${simulatorIdentifierFile})"
             }
     }
 
