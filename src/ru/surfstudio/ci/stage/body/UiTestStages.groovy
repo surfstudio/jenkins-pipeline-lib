837452272c849116a286c40e0a11f58ae30d2612
itolubaev <itolubaev@surfstudio.co>
2018-06-15 20:07:59 +0300

    no message

--- src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
+++ src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
@@ -152,7 +152,8 @@ class UiTestStages {
             script.echo "Setting up simulator ..."
             script.sh "xcrun simctl create \"MyTestiPhone\" \"${device}\" \"${iosVersion}\" > ${simulatorIdentifierFile}"        
             script.sh "xcrun simctl boot \$(cat ${simulatorIdentifierFile})"
-            script.sh "xcrun simctl install booted ${derivedDataPath}/Build/Products/Debug-iphonesimulator/*.app"
+        }
+            script.sh "xcrun simctl install booted ${derivedDataPath}/Build/Products/Debug-iphonesimulator/\$().app"
             
             script.echo "Tests started"
             script.echo "start tests for $taskKey"
@@ -161,7 +162,7 @@ class UiTestStages {
             }
             
             try {
-                script.sh "APP_BUNDLE_PATH=${derivedDataPath}/Build/Products/Debug-iphonesimulator/*.app DEVICE_TARGET=\$(cat ${simulatorIdentifierFile}) bundle exec cucumber -p ios ${featuresDir}/${featureFile} -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile} -f pretty"
+                script.sh "APP_BUNDLE_PATH=${derivedDataPath}/Build/Products/Debug-iphonesimulator/\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//').app DEVICE_TARGET=\$(cat ${simulatorIdentifierFile}) bundle exec cucumber -p ios ${featuresDir}/${featureFile} -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile} -f pretty"
             } finally {
                 script.echo "Removing simulator ..."
                 script.sh "xcrun simctl delete \$(cat ${simulatorIdentifierFile})"
