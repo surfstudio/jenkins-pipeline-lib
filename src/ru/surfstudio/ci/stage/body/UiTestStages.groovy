0d2a569fe2761a371cf50df25509e9498fc60fc2
itolubaev <itolubaev@surfstudio.co>
2018-06-18 12:47:12 +0300

    внес --concurrent

--- src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
+++ src/ru/surfstudio/ci/stage/body/UiTestStages.groovy
@@ -135,7 +135,7 @@ class UiTestStages {
                 script.sh "mkdir $outputsDir"
             }
             //CommonUtil.shWithRuby(script, "calabash-android run ${artifactForTest} -p ${platform} ${featuresDir}/${featureFile} -f pretty -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile}")
-            CommonUtil.shWithRuby(script, "parallel-calabash -a ${artifactForTest} -o \"-p ${platform} -f pretty -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile}\" ${featuresDir}/${featureFile}" --concurrent)
+            CommonUtil.shWithRuby(script, "parallel-calabash -a ${artifactForTest} -o \"-p ${platform} -f pretty -f html -o ${outputsDir}/${outputHtmlFile} -f json -o ${outputsDir}/${outputJsonFile}\" ${featuresDir}/${featureFile} --concurrent")
 
         //AndroidUtil.onEmulator(script, "avd-main"){
            
