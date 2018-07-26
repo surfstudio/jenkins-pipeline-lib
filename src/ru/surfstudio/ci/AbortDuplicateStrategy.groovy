25d71ad3bf313624bf6cae3c4dfe2fc9af630e80
Tuev Max <tuev@surfstudio.ru>
2018-07-26 15:46:17 +0300

    fix

--- src/ru/surfstudio/ci/AbortDuplicateStrategy.groovy
+++ src/ru/surfstudio/ci/AbortDuplicateStrategy.groovy
@@ -2,6 +2,6 @@
 package ru.surfstudio.ci
 
 class AbortDuplicateStrategy {
-    public static final String SELF = 'SELF'
-    public static final String ANOTHER = 'ANOTHER'
+    public static final String SELF = 'SELF'       //отменить текущий билд если есйчас выполняется другой такой же
+    public static final String ANOTHER = 'ANOTHER' //отменить другие такие же билды, если они выполняются
 }
