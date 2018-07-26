f1a502bbc45cf9d2d1127dd6590482bcda1e6868
Tuev Max <tuev@surfstudio.ru>
2018-07-26 13:35:08 +0300

    fix

---
+++ src/ru/surfstudio/ci/pipeline/TestAutoAbortedPipeline.groovy
@@ -0,0 +1,54 @@
+package ru.surfstudio.ci.pipeline
+
+import ru.surfstudio.ci.AbortDublicateStrategy
+import ru.surfstudio.ci.CommonUtil
+import ru.surfstudio.ci.NodeProvider
+import ru.surfstudio.ci.stage.StageStrategy
+
+import static ru.surfstudio.ci.CommonUtil.applyParameterIfNotEmpty
+
+abstract class TestAutoAbortedPipeline extends AutoAbortedPipeline {
+
+
+    def String branchName = ""
+
+    TestAutoAbortedPipeline(Object script) {
+        super(script)
+    }
+
+    @Override
+    def initInternal() {
+
+        //configuration
+        node = NodeProvider.getAndroidNode()
+
+        preExecuteStageBody = {}
+        postExecuteStageBody = {}
+
+
+        stages = [
+                createStage("Init", StageStrategy.FAIL_WHEN_STAGE_ERROR){
+                    applyParameterIfNotEmpty(script, "branchName", script.paramsbranchName.branchName, {
+                        value -> this.branchName = value
+                    })
+                },
+                createStage("Checkout", StageStrategy.FAIL_WHEN_STAGE_ERROR){
+
+                },
+                createStage("IntegrationTest", StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+
+                }
+        ]
+
+        finalizeBody = {
+            script.currentBuild.result = "NOT_BUILT"
+        }
+    }
+
+    @Override
+    def final String getBuildIdentifier() {
+        return branchName
+    }
+
+
+}
