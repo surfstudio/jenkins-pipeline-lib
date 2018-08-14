2cdb94e35b8dc934e4954d6b215a5a3073be3d3b
Tuev Max <tuev@surfstudio.ru>
2018-08-14 19:21:43 +0300

    fix bitbucket notify

--- src/ru/surfstudio/ci/CommonUtil.groovy
+++ src/ru/surfstudio/ci/CommonUtil.groovy
@@ -65,7 +65,7 @@ class CommonUtil {
 
     def static getCurrentBitbucketCommitSHA1(Object script, String gitDir='') {
         script.dir(gitDir) {
-            script echo "${script.sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()}"
+            script.echo "${script.sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()}"
             return script.sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
         }
     }
