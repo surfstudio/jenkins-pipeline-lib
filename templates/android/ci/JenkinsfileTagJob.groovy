9f772e678ffa89ab5efca500c6b593ae00553e4b
Tuev Max <tuev@surfstudio.ru>
2018-08-21 11:15:06 +0300

    refactor pr pipeline template

--- templates/android/ci/JenkinsfileTagJob.groovy
+++ templates/android/ci/JenkinsfileTagJob.groovy
@@ -6,6 +6,11 @@ import ru.surfstudio.ci.stage.StageStrategy
 def pipeline = new TagPipelineAndroid(this)
 pipeline.init()
 
+//configuration
+//TODO: set real values ↓↓↓ (mechanism see in AndroidUtil.withKeystore)
+pipeline.keystoreCredentials = null
+pipeline.keystorePropertiesCredentials = null
+
 //customization
 pipeline.getStage(pipeline.INSTRUMENTATION_TEST).strategy = StageStrategy.SKIP_STAGE
 pipeline.getStage(pipeline.STATIC_CODE_ANALYSIS).strategy = StageStrategy.SKIP_STAGE
