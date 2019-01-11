6b948150d3d490d555227fd97db3c6bd44d81599
Сергей Лоскутов <loskutov@surfstudio.ru>
2019-01-11 16:26:12 +0000

    UiTestPipelineiOS.groovy edited online with Bitbucket

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -118,7 +118,7 @@ class UiTestPipelineiOS extends UiTestPipeline {
             }
             CommonUtil.shWithRuby(script, "bundle install")
             //CommonUtil.shWithRuby(script, "echo -ne '\n' | bundle exec calabash-ios setup ${sourcesDir}")
-            script.sh "expect ../calabash-expect.sh"
+            script.sh "cd .. && expect calabash-expect.sh"
             script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//') -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
         }
     }
