cdd43010cfafd3488b0861663ce5e316d1aedd7d
tolubaev-surf <tolubaev@surfstudio.co>
2019-02-08 12:03:46 +0300

    return calabash-ios setup before build

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -117,12 +117,13 @@ class UiTestPipelineiOS extends UiTestPipeline {
                 CommonUtil.shWithRuby(script, "make init")
             }
 
-
-            script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
-            
             CommonUtil.shWithRuby(script, "bundle install")
             
             CommonUtil.shWithRuby(script, "set -x; expect -f calabash-expect.sh; set +x;")
+
+            script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
+            
+            
         }
     }
 
