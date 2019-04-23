cde852879802b50315c3879132a428cfdd34eb38
tuev <tuev@surfstudio.ru>
2019-04-23 17:13:06 +0300

    refactor

--- src/ru/surfstudio/ci/stage/NodeStagesWrapper.groovy
+++ src/ru/surfstudio/ci/stage/NodeStagesWrapper.groovy
@@ -6,28 +6,30 @@ package ru.surfstudio.ci.stage
 public class NodeStagesWrapper extends AbstractStagesWrapper {
     String node
     boolean copyWorkspace = true
-    static final def nodeWrapperFunction = {
-        script, executeStagesBody ->
-            def stashName = "${script.env.JOB_NAME}_${script.env.BUILD_NUMBER}_workspace"
-            if(copyWorkspace){
-                script.stash includes: '**', name: stashName
-            }
-            script.node(node) {
-                script.echo "Switch to node ${node}: ${script.env.NODE_NAME}"
-                if(copyWorkspace) {
-                    script.unstash stashName
-                }
-                executeStagesBody()
-            }
-            script.echo "Switch back to previous node: ${script.env.NODE_NAME}"
-
-    }
-
-
 
     public NodeStagesWrapper(String name, String node, boolean copyWorkspace, List<Stage> stages) {
-        super(name, nodeWrapperFunction, stages)
+        super(name, {}, stages)
         this.node = node
         this.copyWorkspace = copyWorkspace
+        this.stagesWrapperFunction = {
+            script, executeStagesBody ->
+                def stashName = "${script.env.JOB_NAME}_${script.env.BUILD_NUMBER}_workspace"
+                if(copyWorkspace){
+                    script.stash includes: '**', name: stashName
+                }
+                try {
+                    script.node(node) {
+                        script.echo "Switch to node ${node}: ${script.env.NODE_NAME}"
+                        if (copyWorkspace) {
+                            script.unstash stashName
+                        }
+                        executeStagesBody()
+                    }
+                } finally {
+                    script.echo "Switch back to previous node: ${script.env.NODE_NAME}"
+                }
+
+        }
+
     }
 }
