95ac68707007764c4dd3e89da944d8726c019813
tuev <tuev@surfstudio.ru>
2019-04-25 17:07:37 +0300

    fix pr job

--- src/ru/surfstudio/ci/pipeline/pr/PrPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrPipeline.groovy
@@ -73,6 +73,9 @@ abstract class PrPipeline extends ScmPipeline {
         if(ctx.targetBranchChanged) {
             script.echo "Build triggered by target branch changes, run only ${ctx.stagesForTargetBranchChangedMode} stages"
             ctx.forStages { stage ->
+                if(!stage instanceof SimpleStage){
+                    return 
+                }
                 def executeStage = false
                 for(stageNameForTargetBranchChangedMode in ctx.stagesForTargetBranchChangedMode){
                     executeStage = executeStage || (stageNameForTargetBranchChangedMode == stage.getName())
