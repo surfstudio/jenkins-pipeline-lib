8b87b95fd11d385b0fac306dba5bd9642cc7cd2b
Артем Зайцев <zaytsev@surfstudio.ru>
2019-05-21 14:38:28 +0000

    change set +x to set -x

--- src/ru/surfstudio/ci/CommonUtil.groovy
+++ src/ru/surfstudio/ci/CommonUtil.groovy
@@ -87,7 +87,7 @@ class CommonUtil {
     //endregion
 
     def static shWithRuby(Object script, String command, String version = "2.3.5") {
-        script.sh "hostname; set +x; source ~/.bashrc; source ~/.rvm/scripts/rvm; rvm use $version; $command"
+        script.sh "hostname; set -x; source ~/.bashrc; source ~/.rvm/scripts/rvm; rvm use $version; $command"
     }
 
     @Deprecated
