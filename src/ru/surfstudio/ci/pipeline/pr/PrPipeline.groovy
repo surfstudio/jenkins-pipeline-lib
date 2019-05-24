b63014c1bddd29e9231d78336e0011572f8546f2
Сергей Лоскутов <loskutov@surfstudio.ru>
2019-05-24 10:05:32 +0000

    fix pr pipeline regexp filter expression

--- src/ru/surfstudio/ci/pipeline/pr/PrPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrPipeline.groovy
@@ -215,7 +215,7 @@ abstract class PrPipeline extends ScmPipeline {
                         printContributedVariables: true,
                         printPostContent: true,
                         causeString: 'Triggered by Bitbucket',
-                        regexpFilterExpression: "$repoUrl",
+                        regexpFilterExpression: '^'+"$repoUrl"+'$',
                         regexpFilterText: '$repoUrl'
                 ),
                 script.pollSCM('')
