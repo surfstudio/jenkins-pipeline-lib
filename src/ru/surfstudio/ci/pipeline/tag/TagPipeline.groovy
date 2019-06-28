b77259d31d013557127e81933d5aaab34b22e62d
zaytsev <zaytsev@srfstudio.ru>
2019-06-28 11:44:31 +0300

    add build type as choice parameter

--- src/ru/surfstudio/ci/pipeline/tag/TagPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/tag/TagPipeline.groovy
@@ -23,7 +23,7 @@ import ru.surfstudio.ci.Result
 
 import ru.surfstudio.ci.pipeline.ScmPipeline
 import ru.surfstudio.ci.stage.SimpleStage
-import ru.surfstudio.ci.stage.StageStrategy
+
 import java.util.regex.Pattern
 
 import static ru.surfstudio.ci.CommonUtil.extractValueFromEnvOrParamsAndRun
@@ -180,7 +180,10 @@ abstract class TagPipeline extends ScmPipeline {
     public static final String STATIC_CODE_ANALYSIS_STAGE_STRATEGY_PARAMETER = 'staticCodeAnalysisStageStrategy'
     public static final String BETA_UPLOAD_STAGE_STRATEGY_PARAMETER = 'betaUploadStageStrategy'
     public static final String REPO_TAG_PARAMETER = 'repoTag_0'
-    public static final String IOS_BUILD_TYPE= 'iosBuildType'
+    public static final String BUILD_TYPE = 'iosBuildType'
+
+    public static final String RELEASE_TYPE = "release"
+    public static final String QA_TYPE = "qa"
 
     public static final String STAGE_STRATEGY_PARAM_DESCRIPTION = 'stage strategy types, see repo <a href="https://bitbucket.org/surfstudio/jenkins-pipeline-lib">jenkins-pipeline-lib</a> , class StageStrategy. If empty, job will use initial strategy for this stage'
 
@@ -213,19 +216,13 @@ abstract class TagPipeline extends ScmPipeline {
                         selectedValue: 'NONE',
                         sortMode: 'DESCENDING_SMART'
                 ],
-/*                [
-                        name: IOS_BUILD_TYPE,
-                        description: "Вариант билда",
-                        choice: ["release\nqa"],
-                        selectedValue: 'NONE',
-                ],*/
                 script.choice(
-                        name: 'Nodes',
-                        choices:"Linux\nMac",
-                        description: "Choose Node!"),
-//                script.string(
-//                        name: UNIT_TEST_STAGE_STRATEGY_PARAMETER,
-//                        description: STAGE_STRATEGY_PARAM_DESCRIPTION),
+                        name: BUILD_TYPE,
+                        choices: [RELEASE_TYPE, QA_TYPE],
+                        description: "Выберите тип сборки. Для iOS release загружается в тестфлайт"),
+                script.string(
+                        name: UNIT_TEST_STAGE_STRATEGY_PARAMETER,
+                        description: STAGE_STRATEGY_PARAM_DESCRIPTION),
                 script.string(
                         name: INSTRUMENTATION_TEST_STAGE_STRATEGY_PARAMETER,
                         description: STAGE_STRATEGY_PARAM_DESCRIPTION),
