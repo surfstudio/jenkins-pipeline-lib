addf7c117f2bfec51f9fe5854c94d07705a6b86b
zaytsev <zaytsev@srfstudio.ru>
2019-07-18 18:46:36 +0300

    add todo

--- src/ru/surfstudio/ci/pipeline/tag/TagPipelineFlutter.groovy
+++ src/ru/surfstudio/ci/pipeline/tag/TagPipelineFlutter.groovy
@@ -63,7 +63,7 @@ class TagPipelineFlutter extends TagPipeline {
     public configFile = "pubspec.yaml"
     public compositeVersionNameVar = "version"
 
-    public shBetaUploadCommandAndroid = "cd android && fastlane android beta"
+    public shBetaUploadCommandAndroid = "cd android && fastlane android beta" //todo android release build?
     public shBetaUploadCommandIos = "make -C ios/ beta"
     public shTestFlightUploadCommandIos = "make -C ios/ release"
 
