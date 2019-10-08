6cbb11e690638f0e3e55eddd8bb91b8aa6c18c5a
Игорь Толубаев <tolubaev@surfstudio.co>
2019-10-08 20:18:30 +0000

    UiTestPipelineiOS.groovy edited online with Bitbucket

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -136,7 +136,7 @@ class UiTestPipelineiOS extends UiTestPipeline {
     //            CommonUtil.shWithRuby(script, "ruby ${sourcesDir}/scripts/flatter.rb ${sourcesDir}/*.xcodeproj")
   //          }
             
-            CommonUtil.shWithRuby(script, "bundle exec ruby ${sourcesDir}/scripts/flatter.rb ${sourcesDir}/*.xcodeproj")
+
             script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
             
             
