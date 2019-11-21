eeb43c285a7c27acaaa5b2bd85bcde53f33c403e
zaytsev <zaytsev@surfstudio.ru>
2019-11-21 16:09:49 +0300

    [hotfix] - return  name of stage groups, skip stages except fakes

--- src/ru/surfstudio/ci/pipeline/tag/TagPipelineFlutter.groovy
+++ src/ru/surfstudio/ci/pipeline/tag/TagPipelineFlutter.groovy
@@ -287,7 +287,7 @@ class TagPipelineFlutter extends TagPipeline {
         ]
         def additionalParams = [:]
         if (!ctx.shouldBuildAndroid) {
-            for (stage in ctx.androidStages.dropWhile {stage -> stage.name == STAGE_ANDROID}) {
+            for (stage in ctx.androidStages.dropWhile { s -> s.name == STAGE_ANDROID}) {
                 additionalParams += [
                         (stage.name) : skipResolver(true)
                 ]
@@ -295,7 +295,7 @@ class TagPipelineFlutter extends TagPipeline {
         }
 
         if (!ctx.shouldBuildIos) {
-            for (stage in ctx.iosStages.dropWhile {stage -> stage.name == STAGE_IOS}) {
+            for (stage in ctx.iosStages.dropWhile { s -> s.name == STAGE_IOS}) {
                 additionalParams += [
                         (stage.name) : skipResolver(true)
                 ]
