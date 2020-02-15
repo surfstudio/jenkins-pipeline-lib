963a94d278581e16a5db1417883f93895cbc9403
Даниил Осипов <dan4ex@yandex.ru>
2020-02-15 12:08:38 +0300

    Test pipeline

--- src/ru/surfstudio/ci/RepositoryUtil.groovy
+++ src/ru/surfstudio/ci/RepositoryUtil.groovy
@@ -62,7 +62,7 @@ class RepositoryUtil {
             script.error("You must call RepositoryUtil.saveCurrentGitCommitHash() before invoke this method")
         }
         script.echo "Notify GitLab - stage: $stageName, repoSlug: $slug, commitId: $commit, status: $result"
-        script.updateGitlabCommitStatus(name: "$stageName", state: "$gitlabStatus", builds: [[projectId: "$slug", revisionHash: PrPipeline.sourceBranch]])
+        script.updateGitlabCommitStatus(name: "$stageName", state: "$gitlabStatus", builds: [[projectId: "$slug", revisionHash: Pipeline.sourceBranch]])
     }
 
     def static notifyBitbucketAboutStageStart(Object script, String repoUrl, String stageName){
