d3ee57aa56b332b668dd00c5a9a260fb4d52a0bc
Игорь Толубаев <tolubaev@surfstudio.ru>
2020-03-12 10:08:42 +0000

    UiTestPipeline.groovy edited online with Bitbucket

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
@@ -58,7 +58,7 @@ abstract class UiTestPipeline extends ScmPipeline {
     public outputHtmlFile = "report.html"
     public outputrerunTxtFile = "rerun.txt"
     public outputsIdsDiff = "miss_id.txt"
-    public failedSteps = "failed_steps.txt"
+    public failedStepsFile = "failed_steps.txt"
 
     //credentials
     public jiraAuthenticationName = 'Jarvis_Jira'
@@ -219,7 +219,7 @@ abstract class UiTestPipeline extends ScmPipeline {
                                        String outputrerunTxtFile, 
                                        String jiraAuthenticationName,
                                        String htmlReportName,
-                                       String failedSteps) {
+                                       String failedStepsFile) {
         script.dir(outputsDir) {
             //def testResult = script.readFile file: outputJsonFile
             //script.echo "Test result json: $testResult"
@@ -236,7 +236,7 @@ abstract class UiTestPipeline extends ScmPipeline {
                 script.sh "cd .. && curl -H \"Content-Type: multipart/form-data\" -u ${script.env.USERNAME}:${script.env.PASSWORD} -F \"file=@arhive.zip\" ${Constants.JIRA_URL}rest/raven/1.0/import/execution/bundle"
             }
             
-            CommonUtil.shWithRuby(script, "set -x; ruby -r './Scripts/group_sters.rb' e \'GroupScenarios.new.group_failed_scenarios(${outputJsonFile}, ${failedSteps}\')")
+            CommonUtil.shWithRuby(script, "set -x; ruby -r './Scripts/group_sters.rb' e \'GroupScenarios.new.group_failed_scenarios(${outputJsonFile}, ${failedStepsFile}\')")
             
             script.step([$class: 'ArtifactArchiver', artifacts: outputrerunTxtFile, allowEmptyArchive: true])
             
