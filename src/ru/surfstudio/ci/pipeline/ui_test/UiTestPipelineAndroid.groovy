c5b6505cfcee1dfad5a52650ac34e6198fbbe746
Даниил Осипов <osipov@surfstudio.ru>
2020-03-13 11:40:16 +0000

    UiTestPipelineAndroid.groovy edited online with Bitbucket

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
@@ -104,7 +104,7 @@ class UiTestPipelineAndroid extends UiTestPipeline {
                  
                 script.step ([$class: 'CopyArtifact',
                     projectName: 'Labirint_Android_TAG',
-                    filter: "app/build/outputs/apk/qa/LabirintAndroid-1.7.0-rc09-(3009)-qa.apk",
+                    filter: "app/build/outputs/apk/qa/LabirintAndroid-1.7.0-rc09*",
                     target: "${sourcesDir}"]);
             }
         }
