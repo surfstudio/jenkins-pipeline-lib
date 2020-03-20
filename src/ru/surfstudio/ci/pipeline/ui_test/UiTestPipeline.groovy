0e6dac435bd59b33a0a3e79145ff0295fed2e411
Игорь Толубаев <tolubaev@surfstudio.co>
2020-03-20 14:15:39 +0300

    Fix parameter projectForBuild

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
@@ -413,7 +413,7 @@ abstract class UiTestPipeline extends ScmPipeline {
 
     }
 
-    private static void parameters(script, String defaultTaskKey, String testBranch, String defaultSourceBranch, String node) {
+    private static void parameters(script, String defaultTaskKey, String testBranch, String defaultSourceBranch, String projectForBuild, String node) {
         return script.parameters([
                 script.string(
                         name: TASK_KEY_PARAMETER,
