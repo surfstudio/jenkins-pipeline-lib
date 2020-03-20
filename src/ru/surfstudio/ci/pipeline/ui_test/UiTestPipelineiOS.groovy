377f4ac4e0611739668b384c17ece1f05d783a38
Игорь Толубаев <tolubaev@surfstudio.co>
2020-03-21 01:43:32 +0300

    force move .app

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -163,7 +163,7 @@ class UiTestPipelineiOS extends UiTestPipeline {
             script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
             //script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -derivedDataPath ${derivedDataPath}"
           
-            script.sh "mv src/Build/Products/Debug-iphonesimulator/Labirint-cal.app ." 
+            script.sh "mv -f src/Build/Products/Debug-iphonesimulator/Labirint-cal.app ." 
             //script.dir(sourcesDir){ 
                 script.sh "zip -r Labirint-cal.app.zip Labirint-cal.app"
                 script.step([$class: 'ArtifactArchiver', artifacts: "Labirint-cal.app.zip"])
