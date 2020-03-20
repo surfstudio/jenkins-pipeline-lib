9bfe7192b788532a7dcbad79fa670783a57533ad
Игорь Толубаев <tolubaev@surfstudio.co>
2020-03-21 00:08:48 +0300

    move Lab.app to root

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -162,7 +162,10 @@ class UiTestPipelineiOS extends UiTestPipeline {
 
             script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
             //script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -derivedDataPath ${derivedDataPath}"
-            //script.step([$class: 'ArtifactArchiver', artifacts: "${derivedDataPath}/Build/Products/Debug-iphonesimulator/Labirint-cal.app"])
+            script.dir(sourcesDir) {
+                script.sh "mv Build/Products/Debug-iphonesimulator/Labirint-cal.app Labirint-cal.app" 
+            }
+            script.step([$class: 'ArtifactArchiver', artifacts: "Labirint-cal.app"])
 
         //def files = script.findFiles(glob: builtApkPattern)
         //String foundedApks = files.join("\n")
