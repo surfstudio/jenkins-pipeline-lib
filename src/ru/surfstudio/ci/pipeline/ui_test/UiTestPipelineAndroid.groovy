910e29a20020418f099df37783e9110ebf7c6788
Игорь Толубаев <tolubaev@surfstudio.co>
2020-03-23 18:32:03 +0300

    change selector's syntax

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
@@ -100,7 +100,7 @@ class UiTestPipelineAndroid extends UiTestPipeline {
                     script.step ([$class: 'CopyArtifact',
                     projectName: "Labirint_Android_UI_TEST",
                     filter: "miss_id.txt",
-                    selector: "lastCompleted",
+                    selector: specific:("lastCompleted"),
                     target: "${sourcesDir}"])
                  }}
              else 
