a12e3853db1204e6b80904865278dd68062f1132
Игорь Толубаев <tolubaev@surfstudio.co>
2020-03-24 17:57:22 +0300

    debugging UITestPipeline

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineiOS.groovy
@@ -155,7 +155,8 @@ class UiTestPipelineiOS extends UiTestPipeline {
             CommonUtil.shWithRuby(script, "set -x; expect -f calabash-expect.sh; set +x;")
             
             script.sh "xcodebuild -workspace ${sourcesDir}/*.xcworkspace -scheme \"\$(xcodebuild -workspace ${sourcesDir}/*.xcworkspace -list | grep '\\-cal' | sed 's/ *//')\" -allowProvisioningUpdates -sdk ${sdk} -derivedDataPath ${derivedDataPath}"
-            script.step([$class: 'ArtifactArchiver', artifacts: "**/*-cal.app"])
+            script.sh "cd ${sourcesDir}/Build/Products/Debug-iphonesimulator/; ls"
+            script.step([$class: 'ArtifactArchiver', artifacts: "${sourcesDir}/Build/Products/Debug-iphonesimulator/Labirint-cal.app"])
         }
     }
 
