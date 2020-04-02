9e813fc89133c91dc9b5bbda33ba16dbf156efc7
Игорь Толубаев <tolubaev@surfstudio.co>
2020-04-02 18:39:47 +0300

    add echo

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipelineAndroid.groovy
@@ -36,7 +36,7 @@ class UiTestPipelineAndroid extends UiTestPipeline {
 
     private static void launchEmulator(Object script, AvdConfig config) {
         script.sh "source ~/.bashrc; yes | ${CommonUtil.getSdkManagerHome(script)} \"${config.sdkId}\""
-        CommonUtil.getAndroidHome(script)
+        script.echo CommonUtil.getAndroidHome(script)
         EmulatorUtil.createAndLaunchNewEmulator(script, config)
     }
 
