57b9d868b041bac84e93f219e2ff642df282f193
Игорь Толубаев <tolubaev@surfstudio.co>
2020-04-02 13:09:21 +0300

    Fix ENVIRONMENT_PARAMETER

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
@@ -353,7 +353,7 @@ abstract class UiTestPipeline extends ScmPipeline {
     public static final String SOURCE_BRANCH_PARAMETER = 'sourceBranch'
     public static final String PROJECT_FOR_BUILD_PARAMETER = 'projectForBuild'
     public static final String USER_EMAIL_PARAMETER = 'userEmail'
-    public static final Boolean ENVIRONMENT_PARAMETER = false
+    public static final String ENVIRONMENT_PARAMETER = 'environment'
     public static final String NODE_PARAMETER = 'node'
 
 
