d1c9d75e64ab9b1c2bbdb259236df8b29f53df33
Даниил Осипов <dan4ex@yandex.ru>
2020-04-02 11:50:31 +0300

    Test pipeline with new parameter environment

--- src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
+++ src/ru/surfstudio/ci/pipeline/ui_test/UiTestPipeline.groovy
@@ -157,7 +157,6 @@ abstract class UiTestPipeline extends ScmPipeline {
         extractValueFromEnvOrParamsAndRun(script, USER_EMAIL_PARAMETER) {
             value -> ctx.userEmail = value
         }
-
         extractValueFromBoolParamsAndRun(script, ENVIRONMENT_PARAMETER) {
             value -> ctx.environment = value
         }
