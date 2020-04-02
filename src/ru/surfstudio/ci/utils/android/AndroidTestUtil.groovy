80751e0178e613f9549a7673bac1334e5e0ee2f6
Игорь Толубаев <tolubaev@surfstudio.co>
2020-04-02 20:59:23 +0300

    return methods do private

--- src/ru/surfstudio/ci/utils/android/AndroidTestUtil.groovy
+++ src/ru/surfstudio/ci/utils/android/AndroidTestUtil.groovy
@@ -107,12 +107,12 @@ class AndroidTestUtil {
     }
 
     //region Stages of instrumental tests running
-    public static void launchEmulator(Object script, AvdConfig config) {
+    private static void launchEmulator(Object script, AvdConfig config) {
         script.sh "yes | ${CommonUtil.getSdkManagerHome(script)} \"${config.sdkId}\""
         EmulatorUtil.createAndLaunchNewEmulator(script, config)
     }
 
-    public static void checkEmulatorStatus(Object script, AvdConfig config) {
+    private static void checkEmulatorStatus(Object script, AvdConfig config) {
         if (EmulatorUtil.isEmulatorOffline(script, config.emulatorName) || !CommonUtil.isNotNullOrEmpty(config.emulatorName)) {
             EmulatorUtil.closeAndCreateEmulator(script, config, "emulator is offline")
         } else {
