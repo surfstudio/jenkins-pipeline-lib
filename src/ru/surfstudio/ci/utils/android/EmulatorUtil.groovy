a13133a0b0c7bd26d4275e0b4f57ab1d1152f157
Игорь Толубаев <tolubaev@surfstudio.co>
2020-04-10 20:31:57 +0300

    add '&' to emulator's launch

--- src/ru/surfstudio/ci/utils/android/EmulatorUtil.groovy
+++ src/ru/surfstudio/ci/utils/android/EmulatorUtil.groovy
@@ -164,7 +164,7 @@ class EmulatorUtil {
     private static String getAvdNameForEmulatorPort(Object script, String emulatorPort) {
         def netcatOutput = getShCommandOutput(
                 script,
-                "echo avd name | nc localhost $emulatorPort"
+                "echo avd name | nc localhost $emulatorPort &"
         ).split()
         return netcatOutput[netcatOutput.length - 2]
     }
