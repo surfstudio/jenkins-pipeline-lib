e073442e88a22debc0179ca33569c4c5001dc0f4
Алксей Скакун <skakun@surfstudio.ru>
2020-05-18 12:04:14 +0300

    I'm trying to get how it works.

---
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -0,0 +1,61 @@
+package ru.surfstudio.ci.pipeline.pr
+
+import ru.surfstudio.ci.NodeProvider
+import ru.surfstudio.ci.RepositoryUtil
+import ru.surfstudio.ci.pipeline.helper.AndroidPipelineHelper
+import ru.surfstudio.ci.pipeline.helper.BackendPipelineHelper
+import ru.surfstudio.ci.stage.StageStrategy
+
+class PrBackend extends PrPipeline {
+    private boolean hasChanges = false
+    public buildGradleTask = "clean assembly"
+    public unitTestGradleTask = "test"
+
+    public unitTestResultPathXml = "build/test-results/test/*.xml"
+    public unitTestResultDirHtml = "build/reports/tests/test/"
+
+
+    PrBackend(Object script) {
+        super(script)
+    }
+
+    @Override
+    def init() {
+        node = NodeProvider.backendNode
+
+        preExecuteStageBody = { stage -> preExecuteStageBodyPr(script, stage, repoUrl) }
+        postExecuteStageBody = { stage -> postExecuteStageBodyPr(script, stage, repoUrl) }
+
+        initializeBody = { initBody(this) }
+        propertiesProvider = { properties(this) }
+
+        stages = [
+                stage(CHECKOUT, false) {
+                    checkout(script, repoUrl, sourceBranch, repoCredentialsId)
+                    saveCommitHashAndCheckSkipCi(script, targetBranchChanged)
+                    abortDuplicateBuildsWithDescription(this)
+                },
+                stage(CODE_STYLE_FORMATTING, StageStrategy.SKIP_STAGE) {
+                    AndroidPipelineHelper.ktlintFormatStageAndroid(script, sourceBranch, destinationBranch)
+                    hasChanges = AndroidPipelineHelper.checkChangesAndUpdate(script, repoUrl, repoCredentialsId, sourceBranch)
+                },
+                stage(UPDATE_CURRENT_COMMIT_HASH_AFTER_FORMAT, StageStrategy.SKIP_STAGE, false) {
+                    if (hasChanges) {
+                        RepositoryUtil.saveCurrentGitCommitHash(script)
+                    }
+                },
+                stage(PRE_MERGE) {
+                    mergeLocal(script, destinationBranch)
+                },
+                stage(BUILD) {
+                    BackendPipelineHelper.buildStageBodyBackend(
+                            script, buildGradleTask
+                    )
+                },
+                stage(UNIT_TEST, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+                    AndroidPipelineHelper.unitTestStageBodyAndroid(script, unitTestGradleTask, unitTestResultPathXml, unitTestResultDirHtml)
+                }
+        ]
+        finalizeBody = { finalizeStageBody(this) }
+    }
+}
