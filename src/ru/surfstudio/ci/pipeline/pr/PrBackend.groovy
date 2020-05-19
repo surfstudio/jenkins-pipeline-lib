364cb263939040977a6541c552ab9ec9ddb76b94
Алксей Скакун <skakun@surfstudio.ru>
2020-05-19 17:12:27 +0300

    Try to change path to test

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -11,8 +11,8 @@ class PrBackend extends PrPipeline {
     public buildGradleTask = "clean assemble"
     public unitTestGradleTask = "test"
 
-    public unitTestResultPathXml = "**/test-results/test/*.xml"
-    public unitTestResultDirHtml = "**/reports/tests/test/"
+    public unitTestResultPathXml = "build/test-results/test/*.xml"
+    public unitTestResultDirHtml = "build/reports/tests/test/"
 
 
     PrBackend(Object script) {
