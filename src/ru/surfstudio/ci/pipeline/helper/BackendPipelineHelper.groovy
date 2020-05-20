d3d92e9a1dd94b62e70484e5aeb51333d0de9918
Алксей Скакун <skakun@surfstudio.ru>
2020-05-20 14:15:04 +0300

    show script's class name

--- src/ru/surfstudio/ci/pipeline/helper/BackendPipelineHelper.groovy
+++ src/ru/surfstudio/ci/pipeline/helper/BackendPipelineHelper.groovy
@@ -26,7 +26,7 @@ final class BackendPipelineHelper {
     def static buildStageBodyBackend(Object script, String buildGradleTask) {
         AndroidUtil.withGradleBuildCacheCredentials(script) {
             script.sh "./gradlew ${buildGradleTask}"
-            script.sh "script class name: ${script.class.name}"
+            script.echo "script class name: ${script.class.name}"
         }
     }
 
