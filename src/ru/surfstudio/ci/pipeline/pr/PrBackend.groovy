480343661a4c7d31f059748909149c15a2e1cb86
Алксей Скакун <skakun@surfstudio.ru>
2020-05-20 21:59:47 +0300

    try to use docker agent

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -1,9 +1,12 @@
 package ru.surfstudio.ci.pipeline.pr
 
+import ru.surfstudio.ci.CommonUtil
 import ru.surfstudio.ci.NodeProvider
 import ru.surfstudio.ci.RepositoryUtil
+import ru.surfstudio.ci.Result
 import ru.surfstudio.ci.pipeline.helper.AndroidPipelineHelper
 import ru.surfstudio.ci.pipeline.helper.BackendPipelineHelper
+import ru.surfstudio.ci.stage.Stage
 import ru.surfstudio.ci.stage.StageStrategy
 
 class PrBackend extends PrPipeline {
@@ -59,4 +62,42 @@ class PrBackend extends PrPipeline {
         ]
         finalizeBody = { finalizeStageBody(this) }
     }
+
+    @Override
+    def run() {
+        CommonUtil.fixVisualizingStagesInParallelBlock(script)
+        try {
+            def initStage = stage(INIT, StageStrategy.FAIL_WHEN_STAGE_ERROR, false, createInitStageBody())
+            initStage.execute(script, this)
+            script.node(node) {
+                if (CommonUtil.notEmpty(node)) {
+                    script.echo "Switch to node ${node}: ${script.env.NODE_NAME}"
+                }
+
+                script.docker.image('gradle:6.0.1-jdk11').internal {
+                    for (Stage stage : stages) {
+                        stage.execute(script, this)
+                    }
+                }
+            }
+        } finally {
+            jobResult = calculateJobResult(stages)
+            if (jobResult == Result.ABORTED || jobResult == Result.FAILURE) {
+                script.echo "Job stopped, see reason above ^^^^"
+            }
+            script.echo "Finalize build:"
+            printStageResults()
+            script.echo "Current job result: ${script.currentBuild.result}"
+            script.echo "Try apply job result: ${jobResult}"
+            script.currentBuild.result = jobResult
+            //нельзя повышать статус, то есть если раньше был установлен failed или unstable, нельзя заменить на success
+            script.echo "Updated job result: ${script.currentBuild.result}"
+            if (finalizeBody) {
+                script.echo "Start finalize body"
+                finalizeBody()
+                script.echo "End finalize body"
+            }
+        }
+    }
+
 }
