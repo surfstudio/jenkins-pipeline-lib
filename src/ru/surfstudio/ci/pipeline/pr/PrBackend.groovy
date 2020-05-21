2dae7e3ea5ffa978e20ec6b22e49fa04b0ea7a08
Алксей Скакун <skakun@surfstudio.ru>
2020-05-21 09:02:49 +0300

    try to use docker agent

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -52,52 +52,18 @@ class PrBackend extends PrPipeline {
                     mergeLocal(script, destinationBranch)
                 },
                 stage(BUILD) {
+                    script.docker.image('gradle:6.0.1-jdk11').inside {
                         BackendPipelineHelper.buildStageBodyBackend(
                                 script, buildGradleTask
                         )
+                    }
                 },
                 stage(UNIT_TEST, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+                    script.docker.image('gradle:6.0.1-jdk11').inside {
                         AndroidPipelineHelper.unitTestStageBodyAndroid(script, unitTestGradleTask, unitTestResultPathXml, unitTestResultDirHtml)
+                    }
                 }
         ]
         finalizeBody = { finalizeStageBody(this) }
     }
-
-    @Override
-    def run() {
-        CommonUtil.fixVisualizingStagesInParallelBlock(script)
-        try {
-            def initStage = stage(INIT, StageStrategy.FAIL_WHEN_STAGE_ERROR, false, createInitStageBody())
-            initStage.execute(script, this)
-            script.node(node) {
-                if (CommonUtil.notEmpty(node)) {
-                    script.echo "Switch to node ${node}: ${script.env.NODE_NAME}"
-                }
-
-                script.docker.image('gradle:6.0.1-jdk11').inside {
-                    for (Stage stage : stages) {
-                        stage.execute(script, this)
-                    }
-                }
-            }
-        } finally {
-            jobResult = calculateJobResult(stages)
-            if (jobResult == Result.ABORTED || jobResult == Result.FAILURE) {
-                script.echo "Job stopped, see reason above ^^^^"
-            }
-            script.echo "Finalize build:"
-            printStageResults()
-            script.echo "Current job result: ${script.currentBuild.result}"
-            script.echo "Try apply job result: ${jobResult}"
-            script.currentBuild.result = jobResult
-            //нельзя повышать статус, то есть если раньше был установлен failed или unstable, нельзя заменить на success
-            script.echo "Updated job result: ${script.currentBuild.result}"
-            if (finalizeBody) {
-                script.echo "Start finalize body"
-                finalizeBody()
-                script.echo "End finalize body"
-            }
-        }
-    }
-
 }
