76a6cb68e74e97c08e916cb762e65c656b23208b
Алксей Скакун <skakun@surfstudio.ru>
2020-05-22 14:59:07 +0300

    usage external cache for mvn and gradle

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -57,7 +57,7 @@ class PrBackend extends PrPipeline {
                     }
                 },
                 stage(UNIT_TEST, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
-                    script.docker.image('gradle:6.0.1-jdk11').withRun('-v ~/.m2:/home/gradle/.m2 -v ~/.gradle:/home/gradle/.gradle') {
+                    script.docker.image('gradle:6.0.1-jdk11').inside('-v /home/jenkins/.m2:/home/gradle/.m2 -v /home/jenkins/.gradle:/home/gradle/.gradle') {
                         BackendPipelineHelper.runUnitTests(script, unitTestGradleTask, unitTestResultPathXml, unitTestResultDirHtml)
                     }
                 }
