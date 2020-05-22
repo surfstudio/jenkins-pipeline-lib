be95749d5cb22f1d9fdcbc45250dfbd3c7e58535
Алксей Скакун <skakun@surfstudio.ru>
2020-05-22 16:38:55 +0300

    usage external cache for mvn and gradle

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -24,7 +24,7 @@ class PrBackend extends PrPipeline {
     @Override
     def init() {
         node = NodeProvider.backendNode
-        dockerImage = "gradle:6.0.1-jdk11"
+//        dockerImage = "gradle:6.0.1-jdk11"
 
         preExecuteStageBody = { stage -> preExecuteStageBodyPr(script, stage, repoUrl) }
         postExecuteStageBody = { stage -> postExecuteStageBodyPr(script, stage, repoUrl) }
