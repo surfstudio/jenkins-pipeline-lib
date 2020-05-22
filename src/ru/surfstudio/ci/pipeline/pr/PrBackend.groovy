978b88775c508dbcf1afdd988ebf58b32ca96c79
Алксей Скакун <skakun@surfstudio.ru>
2020-05-22 17:05:31 +0300

    Added short description, removed using jvm11 docker image by default

--- src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
+++ src/ru/surfstudio/ci/pipeline/pr/PrBackend.groovy
@@ -1,12 +1,24 @@
 package ru.surfstudio.ci.pipeline.pr
 
-
 import ru.surfstudio.ci.NodeProvider
 import ru.surfstudio.ci.RepositoryUtil
 import ru.surfstudio.ci.pipeline.helper.AndroidPipelineHelper
 import ru.surfstudio.ci.pipeline.helper.BackendPipelineHelper
 import ru.surfstudio.ci.stage.StageStrategy
 
+/**
+ * In a case when you need running some steps into specific environment (for instance build and run tests using jvm 11)
+ * you can use {@link PrBackend#runInsideDocker} method, previously you have to set image name {@link PrBackend#dockerImage}.<p>
+ * Build and Unit_test steps are wrapped by {@link PrBackend#runInsideDocker} method, therefore if you'll set {@link PrBackend#dockerImage} field
+ * that steps will run inside docker. <p>
+ *     For Instance: <p>
+ * <pre>
+ * def pipeline = new PrBackend(this)
+ * pipeline.init()
+ * pipeline.dockerImage = "gradle:6.0.1-jdk11"
+ * pipeline.run()
+ * </pre>
+ */
 class PrBackend extends PrPipeline {
     private boolean hasChanges = false
     public buildGradleTask = "clean assemble"
@@ -24,8 +36,6 @@ class PrBackend extends PrPipeline {
     @Override
     def init() {
         node = NodeProvider.backendNode
-//        dockerImage = "gradle:6.0.1-jdk11"
-
         preExecuteStageBody = { stage -> preExecuteStageBodyPr(script, stage, repoUrl) }
         postExecuteStageBody = { stage -> postExecuteStageBodyPr(script, stage, repoUrl) }
 
