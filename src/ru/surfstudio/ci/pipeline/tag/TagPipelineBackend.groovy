6cbbc6e144558024102b5248e75348abaad08f1f
Алксей Скакун <skakun@surfstudio.ru>
2020-05-27 14:23:19 +0300

    empty pipeline for backend

---
+++ src/ru/surfstudio/ci/pipeline/tag/TagPipelineBackend.groovy
@@ -0,0 +1,25 @@
+package ru.surfstudio.ci.pipeline.tag
+
+import ru.surfstudio.ci.NodeProvider
+
+class TagPipelineBackend extends TagPipeline {
+    public buildGradleTask = "clean assemble"
+
+    TagPipelineBackend(Object script) {
+        super(script)
+    }
+
+    @Override
+    def init() {
+        node = NodeProvider.backendNode
+        preExecuteStageBody = { stage -> preExecuteStageBodyPr(script, stage, repoUrl) }
+        postExecuteStageBody = { stage -> postExecuteStageBodyPr(script, stage, repoUrl) }
+
+        initializeBody = { initBody(this) }
+        propertiesProvider = { properties(this) }
+
+        stages = [
+        ]
+        finalizeBody = { finalizeStageBody(this) }
+    }
+}
