0bd4f441e8be75f2ff44a3fbc71870427666421b
Алксей Скакун <skakun@surfstudio.ru>
2020-06-13 17:44:09 +0300

    Вернул методы AndroidPipelineHelper.ktlintFormatStageAndroid и AndroidPipelineHelper.checkChangesAndUpdate для обратной совместимости, добавил @Deprecated

--- src/ru/surfstudio/ci/pipeline/helper/AndroidPipelineHelper.groovy
+++ src/ru/surfstudio/ci/pipeline/helper/AndroidPipelineHelper.groovy
@@ -15,7 +15,8 @@
  */
 package ru.surfstudio.ci.pipeline.helper
 
-
+import ru.surfstudio.ci.CommonUtil
+import ru.surfstudio.ci.RepositoryUtil
 import ru.surfstudio.ci.utils.android.AndroidTestUtil
 import ru.surfstudio.ci.utils.android.AndroidUtil
 import ru.surfstudio.ci.utils.android.config.AndroidTestConfig
@@ -137,5 +138,56 @@ class AndroidPipelineHelper {
     def static staticCodeAnalysisStageBody(Object script) {
         script.echo "empty"
         //todo
+    }
+
+    /**
+     * Форматирование исходного кода на котлин
+     */
+    @Deprecated
+    static ktlintFormatStageAndroid(
+            Object script,
+            String sourceBranch,
+            String destinationBranch
+    ) {
+        def files = RepositoryUtil.ktFilesDiffPr(script, sourceBranch, destinationBranch)
+        if (CommonUtil.isEmptyStringArray(files)) {
+            script.echo "No *.kt files for formatting."
+            return
+        }
+        try {
+            AndroidUtil.withGradleBuildCacheCredentials(script) {
+                script.sh "./gradlew ktlintFilesFormat -PlintFiles=\"${files.join("\",\"")}\""
+            }
+        } catch (Exception ex) {
+            script.echo "Formatting exception $ex"
+        }
+    }
+
+
+    @Deprecated
+    static boolean checkChangesAndUpdate(
+            Object script,
+            String repoUrl,
+            String repoCredentialsId,
+            String sourceBranch
+    ) {
+        boolean hasChanges = RepositoryUtil.checkHasChanges(script)
+        if (hasChanges) {
+            RepositoryUtil.notifyGitlabAboutStageAborted(script, repoUrl, RepositoryUtil.SYNTHETIC_PIPELINE_STAGE, sourceBranch)
+
+            String jiraIssueKey
+            try {
+                jiraIssueKey = "\nApplyed for jira issue: ${(RepositoryUtil.getCurrentCommitMessage(script) =~ JIRA_ISSUE_KEY_PATTERN)[0][0]}."
+            } catch (Exception ignored) {
+                jiraIssueKey = ""
+            }
+            String commitHash = RepositoryUtil.getCurrentCommitHash(script).toString().take(8)
+
+            script.sh "git commit -a -m \"Code Formatting $RepositoryUtil.SKIP_CI_LABEL1." + jiraIssueKey + "\nLast formatted commit is $commitHash \""
+            RepositoryUtil.push(script, repoUrl, repoCredentialsId)
+        } else {
+            script.echo "No modification after code formatting."
+        }
+        return hasChanges
     }
 }
