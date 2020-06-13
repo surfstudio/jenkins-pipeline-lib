be969d3f49fe683e3c27bbc9b30711dcb8d199da
Алксей Скакун <skakun@surfstudio.ru>
2020-06-13 20:04:48 +0300

    Добавил описание для tag pipeline backend

--- templates/backend/ci/JenkinsfileTagJob.groovy
+++ templates/backend/ci/JenkinsfileTagJob.groovy
@@ -3,5 +3,7 @@ import ru.surfstudio.ci.pipeline.tag.TagPipelineBackend
 
 def pipeline = new TagPipelineBackend(this)
 pipeline.init()
-pipeline.registryPathAndProjectId = "surf-infrastructure/template/template"
+//TODO pipeline.registryPathAndProjectId = "surf-infrastructure/some/registry/path" you have to specify the path in registry for result images
+//pipeline.dockerImageForBuild = "gradle:6.0.1-jdk11" you can setup docker image for container for building your application
+//pipeline.registryUrl = "eu.gcr.io" you can setup another google cloud registry
 pipeline.run()
